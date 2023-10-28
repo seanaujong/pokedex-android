3e5eaec362f1e66cfc01b4fb63f536ed0a1415e5
seanaujong <seanaujong@gmail.com>
2023-10-27 21:01:37 -0500

    feat: tightened spacing between search bar and pokemon list

--- app/src/main/java/com/seanaujong/pokedex/ui/screen/pokemonlist/PokemonListScreen.kt
+++ app/src/main/java/com/seanaujong/pokedex/ui/screen/pokemonlist/PokemonListScreen.kt
@@ -79,7 +79,6 @@ fun PokemonListScreen(
             ) {
                 viewModel.searchPokemonList(it)
             }
-            Spacer(modifier = Modifier.height(16.dp))
             PokemonList(navController = navController)
         }
     }
